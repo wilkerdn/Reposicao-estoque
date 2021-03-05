67dc8dc85bdecdc23c9a6ccf2b09bdc0191fb313
Wilker Nunes <51373362+wilkerdn@users.noreply.github.com>
2021-03-05 17:47:01 -0300

    Criei o código

---
+++ Código/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#define TAM 5
+#define BAIXO 5
+
+void troca(int estoque[TAM], int tipoX, int tipoY, int qtde){
+	estoque[tipoX] += qtde;
+	estoque[tipoY] -= qtde;
+}
+
+void comprar(int estoque[TAM]){
+	int i;
+	for(i=0; i < TAM; i++){
+		if(estoque[i] < BAIXO){
+			printf("Comprar %d unidades do tipo %d\n",(BAIXO-estoque[i]),i);
+		}
+	}
+}
+
+int main(){
+	int i, j, estoque[TAM];
+	int tipoX,tipoY,qtde;
+	for(i=0;i<TAM;i++){//carregar estoque de tamanho TAM
+            printf("Digite o numero de unidades do tipo %d\n",i);
+            scanf("%d",&estoque[i]);
+	}
+
+	for(i=0; i < TAM; i++){
+            for(j=0; j<TAM; j++){
+                if(estoque[i]<BAIXO){// verificar se TIPO i esta com estoque baixo
+                    tipoX = i;;// se SIM: trocar por um TIPO j que estiver em alta
+                    if(estoque[j]>TAM){
+                        tipoY = j;
+                        qtde = estoque[j] - BAIXO;
+                        troca(estoque,tipoX,tipoY,qtde);
+                    }
+                }
+            }
+    }
+
+	comprar(estoque);
+
+	return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Reposicao-estoque
- Informar a quantidade de um item e retornar a quantidade necessária para alcançar um número mínimo no estoque
+# Reposição estoque
+Informar a quantidade de um item e retornar a quantidade necessária para alcançar um número mínimo no estoque. O programa é capaz de substituir itens que possui uma maior quantidade por outros que estão com o estoque abaixo do mínimo antes de informar a quantidade a ser comprada.
